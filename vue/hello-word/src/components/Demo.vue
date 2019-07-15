8efbb63b3950d004170982b1b0e12e3cbad763bc
sunweixing123 <359561000@qq.com>
2019-07-15 20:04:59 +0800

    vue组件化练习

---
+++ vue/hello-word/src/components/Demo.vue
@@ -0,0 +1,49 @@
+<template>
+    <todoItem ref="headerChild" :todos="todos" :addNewTodo="addNewTodo" :newTodoText="newTodoText" ></todoItem>
+</template>
+
+
+<script>
+import todoItem from './TodoItem';
+export default {
+    data() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    title: 'Do the dishes',
+                },
+                {
+                    id: 2,
+                    title: 'Take out the trash',
+                },
+                {
+                    id: 3,
+                    title: 'Mow the lawn'
+                }
+            ],
+            newTodoText: '',
+        }
+    },
+    components: {
+        todoItem,
+    },
+    methods: {
+        addNewTodo() {
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.$refs.headerChild.newTodoText
+            })
+            this.newTodoText = ''
+        },
+    }
+    
+}
+</script>
+
+
+<style scoped>
+
+
+
+</style>
